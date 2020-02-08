de16663939e483eee9868939bb367b89ac23d9ea
1807212 <1807212@brunel.ac.uk>
2020-02-08 00:03:47 +0000

    Added The Select A Sport Screen On My Branch

--- app/src/main/java/com/example/group_project_2019_20_group_6/MainActivity.java
+++ app/src/main/java/com/example/group_project_2019_20_group_6/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.group_project_2019_20_group_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnTest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTest = findViewById(R.id.btnTest);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,
+                        com.example.group_project_2019_20_group_6.SelectSportScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
