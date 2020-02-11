d4d959b20aad70acce1c02b9bed80682d05ddd8f
AnishShah1803 <1804417@brunel.ac.uk>
2020-02-11 22:10:45 +0000

    added functionality behind the single pay button and edited layout slightly

--- app/src/main/java/com/example/group_project_2019_20_group_6/Payment_choice.java
+++ app/src/main/java/com/example/group_project_2019_20_group_6/Payment_choice.java
@@ -6,22 +6,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
-public class Payment_choice extends AppCompatActivity {
+public class Payment_choice extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_choice);
-
-
+        ImageButton singlepayment = findViewById(R.id.Singlepayer);
+        ImageButton multipayment = findViewById(R.id.multipayers);
+        singlepayment.setOnClickListener(this);
+        multipayment.setOnClickListener(this);
         //Initialising and assigning variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
         //perform itemselectedlistener
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch(menuItem.getItemId())
@@ -46,5 +54,15 @@ public class Payment_choice extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId())
+        {
+            case R.id.Singlepayer:
+                startActivity(new Intent(getApplicationContext(),Singlepayment.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/group_project_2019_20_group_6/Singlepayment.java
@@ -0,0 +1,16 @@
+package com.example.group_project_2019_20_group_6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Singlepayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singlepayment);
+
+    }
+}
