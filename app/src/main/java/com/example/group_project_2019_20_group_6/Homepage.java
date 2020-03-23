00c82b20fc33c6d90d92982197af78becdd8acfe
1807212 <1807212@brunel.ac.uk>
2020-03-23 19:53:23 +0000

    Added Navigation Bar to the other screens

--- app/src/main/java/com/example/group_project_2019_20_group_6/Homepage.java
+++ app/src/main/java/com/example/group_project_2019_20_group_6/Homepage.java
@@ -1,8 +1,13 @@
 package com.example.group_project_2019_20_group_6;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Homepage extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //perform itemselectedlistener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch(menuItem.getItemId())
+                {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),Homepage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.league:
+                        startActivity(new Intent(getApplicationContext(),LeagueMain.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.userscreen:
+                        startActivity(new Intent(getApplicationContext(),Homepage.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+
+
+        });
+
     }
 }
