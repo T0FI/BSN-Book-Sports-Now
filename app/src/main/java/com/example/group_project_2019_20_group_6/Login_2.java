18aea4b7ab766679214cac5d207f546af2bdb2c6
Michael6080 <56486616+Michael6080@users.noreply.github.com>
2020-05-11 01:02:30 +0100

    Michael - Sign up screen initial design done. Screen transition buttons implemented. Needs further design changes, animation added and resolution changing for cross platform capability.

--- app/src/main/java/com/example/group_project_2019_20_group_6/Login_2.java
+++ app/src/main/java/com/example/group_project_2019_20_group_6/Login_2.java
@@ -2,15 +2,50 @@ package com.example.group_project_2019_20_group_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login_2 extends AppCompatActivity {
+
+    Button callSignUp;
+    Button callhome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_2);
+
+
+        callSignUp = findViewById(R.id.signup);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login_2.this,SignUp.class);
+                startActivity(intent);
+
+            }
+        });
+
+        callhome = findViewById(R.id.go);
+
+        callhome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login_2.this,Homepage.class);
+                startActivity(intent);
+
+            }
+        });
+
+                
+
+
+
+
     }
 }
